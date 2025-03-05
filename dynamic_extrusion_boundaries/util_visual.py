10841574a6d88159c9cb200f851c9b345ff187cc
Hadi Rahmaninejad <rahmanin@discovery1.hpc.usc.edu>
2025-03-05 11:20:12 -0800

    revising contour plots utilities

--- dynamic_extrusion_boundaries/util_visual.py
+++ dynamic_extrusion_boundaries/util_visual.py
@@ -1,29 +1,51 @@
-from scipy.ndimage.filters import gaussian_filter
-import matplotlib.pylab as plt
-import seaborn as sns
+from scipy.ndimage import zoom
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 
+def make_equipotential_lines(heatmap_data, line_values, num_line_x, num_line_y, expand_cof=1, label=None, legend=None, color='red', smooth_scale=1.5, **kwargs):
+    """
+    Plots equipotential contour lines based on a given heatmap.
 
-def make_equipotential_lines(heatmap_data, line_values, num_line_x, num_line_y, label=None, legend=None,color='red', smooth_scale=1, **kwargs):
+    Parameters:
+    -----------
+    heatmap_data : np.ndarray
+        A 2D numpy array representing the heatmap data.
+    line_values : list
+        A list of contour levels to be plotted.
+    num_line_x : int
+        Number of grid points along the x-axis.
+    num_line_y : int
+        Number of grid points along the y-axis.
+    expand_cof : float, optional (default=1)
+        Expansion coefficient to scale the heatmap before plotting.
+    label : str, optional
+        Label for the plotted lines (used in the legend).
+    legend : str, optional
+        Not currently used in the function.
+    color : str, optional (default='red')
+        Color of the contour lines.
+    smooth_scale : float, optional (default=1.5)
+        Gaussian smoothing factor applied to the heatmap before contour plotting.
+    **kwargs : dict
+        Additional keyword arguments passed to `plt.contour`.
 
-    '''
-    
-    heatmap_data: numpy matrix
-    
-    values : list of values to plot
-    
-    smooth_scale : how much to smooth the contour line for visualization
-    
-    '''
-    
-    d = gaussian_filter(heatmap_data.to_numpy(), sigma=smooth_scale)
+    Returns:
+    --------
+    line : matplotlib.lines.Line2D or None
+        A reference to the plotted line if `label` is provided, otherwise None.
+    """
 
-    cntr1 = plt.contour(np.linspace(0, num_line_x, num_line_x * smooth_scale),
-
-              np.linspace(0, num_line_y, num_line_y * smooth_scale),
-
-              d, levels=line_values, colors=color, **kwargs)
+    d = zoom(heatmap_data, zoom=expand_cof, order=3)
+    expanded_d = gaussian_filter(d, sigma=smooth_scale)
     
+    cntr1 = plt.contour(
+        np.linspace(0, num_line_x, int(num_line_x * expand_cof)),
+        np.linspace(0, num_line_y, int(num_line_y * expand_cof)),
+        expanded_d, levels=line_values, colors=color, **kwargs
+    )
+
+    line = None
     if label is not None:
         line, = plt.plot([], [], color=color, linestyle='dashed', alpha=0.83, label=label)
 
